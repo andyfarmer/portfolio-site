3d94653f7173fe7cbf482cd2e3b8b4b424282ce5
Andy Farmer <andy.farmer@viabledata.co.uk>
2024-12-09 15:32:27 +0000

    build home page header

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const browserSync = require('browser-sync').create();
 const gulp = require('gulp');
 const nunjucksRender = require('gulp-nunjucks-render');
 const sass = require('gulp-sass')(require('sass'));
+const gulpCopy = require('gulp-copy');
 
 gulp.task('html', function(){
 
@@ -16,9 +17,15 @@ gulp.task('html', function(){
 
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./dist/css'))
-      .pipe(browserSync.stream());
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./dist/css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('images', function() {
+	return gulp.src('./images/**/*')
+		.pipe(gulpCopy('./dist'))
+		.pipe(browserSync.stream());
 });
 
 gulp.task('serve', gulp.series(['html', 'sass'], function(){
@@ -29,7 +36,8 @@ gulp.task('serve', gulp.series(['html', 'sass'], function(){
     }
   });
 
-  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('./images/**/*', gulp.series('images'));
+	gulp.watch('./sass/**/*.scss', gulp.series('sass'));
   gulp.watch('./views/**/*.njk', gulp.series('html'));
   gulp.watch('./dist/*.html').on('change', browserSync.reload);
 
