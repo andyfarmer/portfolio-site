8e6306885db8cf47b257a481cf75010eae9cabea
Andy Farmer <andy.farmer@viabledata.co.uk>
2024-12-16 21:26:43 +0000

    home page responsive mobile

--- js/index.js
+++ js/index.js
@@ -1,30 +1,50 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrollTrigger);
+	let mm = gsap.matchMedia();
+	let mobileView = window.matchMedia('(max-width: 600px)');
+
+	gsap.registerPlugin(ScrollTrigger);
+
+	const speed = mobileView.matches ? 30 : 15;
+
+	console.log(speed);
 
 	gsap.to('#panel-1', {
-		xPercent: -15,
+		xPercent: (0 - speed),
 		ease: 'none',
 		scrollTrigger: {
 			trigger: '#panel-1',
-			scrub: 0.1
+			scrub: true
 		}
 	});
 
 	gsap.to('#panel-2', {
-		xPercent: 15,
+		xPercent: speed,
 		ease: 'none',
 		scrollTrigger: {
 			trigger: '#panel-2',
-			scrub: 0.1
+			scrub: true
 		}
 	});
 
-	gsap.from('.b-skillchart__bar', {
-		opacity: 0,
-		height: 0,
-		duration: 1.3,
-		stagger: 0.05,
-		scrollTrigger: '.b-skillchart__label--2'
+	mm.add('(min-width: 1025px)', () => {
+		gsap.from('.b-skillchart__bar', {
+			opacity: 0,
+			height: 0,
+			duration: 1.3,
+			stagger: 0.05,
+			scrollTrigger: '.b-skillchart__label--2'
+		});
+	});
+
+	mm.add('(max-width: 1024px)', () => {
+		gsap.from('.b-skillchart__bar', {
+			opacity: 0,
+			width: 0,
+			duration: 1.3,
+			stagger: 0.05,
+			scrollTrigger: '#bar-mobile-trigger',
+			clearProps: 'all'
+		});
 	});
 
 });
